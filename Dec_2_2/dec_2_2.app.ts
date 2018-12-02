4373c74cad6c13268cc9b400f36ed41db380c649
Nathaniel Collier <nathaniel.collier@aecc.com>
2018-12-02 15:30:39 -0600

    Made it show the correct answer

--- Dec_2_2/dec_2_2.app.ts
+++ Dec_2_2/dec_2_2.app.ts
@@ -1,5 +1,4 @@
 import { utility } from "../common/utility";
-
 /*
 --- Part Two ---
 Confident that your list of box IDs is complete, you're ready to find the boxes full of prototype fabric.
@@ -29,15 +28,26 @@ async function main(){
     
     for( let i = 0; i < entries.length; ++i ){
         let matchingEntry = entries.filter((val,index)=>{
-            if( index == i ) return false;
-
+            return index != i;
+        }).map((val,index)=>{
             let diff = compareTwoEntries(entries[i],val);
-            if( diff.length <= 1){
-                return true;
-            }
+            return diff;
+        }).filter((val,index)=>{
+            return val.length <= 1
         });
+
         if( matchingEntry.length > 0 ){
+            let firstMatch = matchingEntry[0];
             console.log(`Index: ${i}, Matching entry found: ${JSON.stringify(matchingEntry)}`);
+
+            // to get a string into a char array split on empty string
+            //   see: https://stackoverflow.com/questions/4547609/how-do-you-get-a-string-to-a-character-array-in-javascript
+            let matchingParts = entries[i].split('').filter((val, ind)=>{
+                return firstMatch.filter((val)=>{
+                    return val.pos == ind;
+                }).length == 0;
+            }).join("");
+            console.log(`Matching parts word is: ${matchingParts}`);
         }
     }
 }
